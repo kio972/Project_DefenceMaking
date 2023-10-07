1e3c3f318daf3bc96e76526f8edd7008d0fda973
kio972 <121113387+kio972@users.noreply.github.com>
2023-10-07 19:55:58 +0900

    길타일 손맵적용

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,47 +5,18 @@ using UnityEngine.Tilemaps;
 
 public class Test : MonoBehaviour
 {
-    public TileNode node;
-    public NodeManager nodeManager;
-    public Direction direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Texture texture;
 
-    public Tilemap tilemap;
+    private MaterialPropertyBlock material;
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                // 충돌된 오브젝트 처리
-                GameObject hitObject = hit.collider.gameObject;
-                //Debug.Log("Hit object: " + hitObject.name);
-                TileNode tile = hitObject.GetComponentInParent<TileNode>();
-                if (tile != null)
-                {
-                    TileNode neighborTile = null;
-                    if (tile.neighborNodeDic.TryGetValue(Direction.Left, out neighborTile))
-                        print(neighborTile.transform.position);
-                    if (tile.neighborNodeDic.TryGetValue(Direction.LeftUp, out neighborTile))
-                        print(neighborTile.transform.position);
-                    if (tile.neighborNodeDic.TryGetValue(Direction.LeftDown, out neighborTile))
-                        print(neighborTile.transform.position);
-                    if (tile.neighborNodeDic.TryGetValue(Direction.Right, out neighborTile))
-                        print(neighborTile.transform.position);
-                    if (tile.neighborNodeDic.TryGetValue(Direction.RightUp, out neighborTile))
-                        print(neighborTile.transform.position);
-                    if (tile.neighborNodeDic.TryGetValue(Direction.RightDown, out neighborTile))
-                        print(neighborTile.transform.position);
-                }
-            }
+            material = new MaterialPropertyBlock();
+            material.SetTexture("_MainTex", texture);
+            GetComponent<Renderer>().SetPropertyBlock(material);
         }
     }
 }
