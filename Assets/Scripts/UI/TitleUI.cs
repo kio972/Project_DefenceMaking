441bd5d54dd6ab551390e6b5c5a98a3cdf04de8c
kio972 <121113387+kio972@users.noreply.github.com>
2023-10-30 16:40:07 +0900

    미믹수정

---
+++ Assets/Scripts/UI/TitleUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Spine.Unity;
+
+public class TitleUI : MonoBehaviour
+{
+    public SkeletonGraphic img;
+    private bool clicked = false;
+
+    public void ResetAnimation()
+    {
+        if (!clicked)
+            return;
+
+        clicked = false;
+        img.AnimationState.SetAnimation(1, "Fire", true);
+    }
+
+}
