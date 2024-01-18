8958ecdccb7268827d0f70f57b346a52784aa1cb
kio972 <121113387+kio972@users.noreply.github.com>
2024-01-18 21:39:35 +0900

    방 마나 표기 센터 근사치 계산 추가

--- Assets/Scripts/InGame/CompleteRoom.cs
+++ Assets/Scripts/InGame/CompleteRoom.cs
@@ -10,7 +10,34 @@ public class CompleteRoom
 
     public List<Tile> _IncludeRooms { get => includeRooms; }
     public int _RemainingMana { get { return totalMana - spendedMana; } }
-    public Tile HeadRoom { get { return includeRooms[0]; } }
+    public Tile HeadRoom { get { return CalculateMidTile(); } }
+
+    private Tile CalculateMidTile()
+    {
+        if (includeRooms.Count == 1)
+            return includeRooms[0];
+
+        Vector3 vector = Vector3.zero;
+        foreach (Tile tile in includeRooms)
+            vector += tile.transform.position;
+
+        vector = vector / includeRooms.Count;
+
+        Tile resultTile = includeRooms[0];
+        float minMagnitude = (vector - includeRooms[0].transform.position).magnitude;
+
+        foreach(Tile tile in includeRooms)
+        {
+            float targetMagnitude = (vector - tile.transform.position).magnitude;
+            if(targetMagnitude < minMagnitude)
+            {
+                minMagnitude = targetMagnitude;
+                resultTile = tile;
+            }
+        }
+
+        return resultTile;
+    }
 
     public CompleteRoom(List<Tile> targetTiles, bool singleRoom = false)
     {
