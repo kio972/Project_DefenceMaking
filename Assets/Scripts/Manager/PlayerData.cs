0e678f91f2a7395a56dd16f38404a3395a9d9bca
kio972 <121113387+kio972@users.noreply.github.com>
2024-05-01 01:46:29 +0900

    세이브 파일 작성중

--- Assets/Scripts/Manager/PlayerData.cs
+++ Assets/Scripts/Manager/PlayerData.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public struct TileData
 {
-
+    public int id { get; set; }
+    public int row { get; set; }
+    public int col { get; set; }
+    public int rotation { get; set; }
 }
 
 public class PlayerData
@@ -17,12 +20,4 @@ public class PlayerData
 
     //타일 관련
     public List<TileData> tiles;
-
-
-
-}
-
-public class PlayerDataJson
-{
-
 }
