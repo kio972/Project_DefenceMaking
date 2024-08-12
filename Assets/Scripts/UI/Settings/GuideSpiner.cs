b87c87616d2099ec0d77a11f0e035686c011f5a6
kio972 <121113387+kio972@users.noreply.github.com>
2024-08-13 00:55:30 +0900

    THREEDAYS-276 <설정, 가이드 UI 리소스 최신화>

--- Assets/Scripts/UI/Settings/GuideSpiner.cs
+++ Assets/Scripts/UI/Settings/GuideSpiner.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GuideSpiner : SpinnerButton
 {
+    [SerializeField]
+    List<Image> guideBtns;
+
     [SerializeField]
     List<GameObject> guideInfos;
 
     [SerializeField]
     GameObject mainInfoMenu;
+
+    [SerializeField]
+    private Sprite defaultSprite;
+    [SerializeField]
+    private Sprite selectedSprite;
 
     public void SetActive(bool value)
     {
@@ -17,7 +26,9 @@ public class GuideSpiner : SpinnerButton
 
     public void SetIndex(int index)
     {
+        guideBtns[this.index].sprite = defaultSprite;
         this.index = index;
+        guideBtns[this.index].sprite = selectedSprite;
         SetBtn();
         OnValueChange();
     }
@@ -28,5 +39,15 @@ public class GuideSpiner : SpinnerButton
             info.SetActive(false);
 
         guideInfos[index].SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        SetIndex(0);
+    }
+
+    private void OnEnable()
+    {
+        SetIndex(0);
     }
 }
