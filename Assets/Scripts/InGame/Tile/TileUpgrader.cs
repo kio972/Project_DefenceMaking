e656515d86e5ca1d28b1829b81e22dff2452a9ea
kio972 <121113387+kio972@users.noreply.github.com>
2024-11-03 23:43:50 +0900

    타일 업그레이드 시스템 리소스

---
+++ Assets/Scripts/InGame/Tile/TileUpgrader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileUpgrader : MonoBehaviour
+{
+    private Tile _tile;
+    private Tile tile
+    {
+        get
+        {
+            if (_tile == null)
+                _tile = GetComponent<Tile>();
+            return _tile;
+        }
+    }
+
+    [SerializeField]
+    private Material _upgradeMaterial;
+
+    public void UpgradeTile()
+    {
+        Renderer renderer = transform.GetChild(0)?.GetComponent<Renderer>();
+        if (renderer != null)
+            renderer.material = _upgradeMaterial;
+    }
+}
