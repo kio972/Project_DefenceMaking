cd59af10a15418fe82361f9740db552f35c75bef
Eldusar <bnfblack@gmail.com>
2025-01-17 23:43:06 +0900

    250117_SoundBank_test2

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,43 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID ALERT_RESEARCH_COMPLETE = 3872819091U;
+        static const AkUniqueID ALERT_TIME = 4243488615U;
+        static const AkUniqueID AMB_CAVE = 3755979049U;
+        static const AkUniqueID CARD_MOUSEOVER = 1586336673U;
+        static const AkUniqueID ENEMY_ARCHER_ATTACK = 3450149562U;
+        static const AkUniqueID ENEMY_BOSS_TYPEFARMER_ATTACK = 2598397792U;
+        static const AkUniqueID ENEMY_FARMER_TYPEA_ATTACK = 1226658030U;
+        static const AkUniqueID ENEMY_FARMER_TYPEB_ATTACK = 3127603755U;
+        static const AkUniqueID ENEMY_THIEF_ATTACK = 1904917713U;
+        static const AkUniqueID ENEMY_WARRIOR_ATTACK = 768907175U;
+        static const AkUniqueID MONSTER_GOBLIN_DEAD = 433730394U;
+        static const AkUniqueID MONSTER_GOBLIN_SPAWN = 3234222325U;
+        static const AkUniqueID MONSTER_GOLEM_ATTACK = 3005370083U;
+        static const AkUniqueID MONSTER_GOLEM_DEAD = 977052537U;
+        static const AkUniqueID MONSTER_GOLEM_SPAWN = 3720990804U;
+        static const AkUniqueID MONSTER_MIMIC_ATTACK = 3467401894U;
+        static const AkUniqueID MONSTER_MIMIC_DEAD = 3020778988U;
+        static const AkUniqueID MONSTER_MIMIC_SPAWN = 2151188167U;
+        static const AkUniqueID MONSTER_SLIME_ATTACK = 3767676951U;
+        static const AkUniqueID MONSTER_SLIME_DEAD = 1055640181U;
+        static const AkUniqueID MONSTER_SLIME_SPAWN = 3004343104U;
+        static const AkUniqueID QUEST_PAPER_BURN = 3766261802U;
+        static const AkUniqueID QUEST_PAPER_OPEN = 822059337U;
+        static const AkUniqueID SHOP_CARD_BUY = 3637533423U;
+        static const AkUniqueID SHOP_OPEN = 839522188U;
+        static const AkUniqueID TILE_CLICK = 3030067132U;
+        static const AkUniqueID TRAP_HOME_ATTACK = 3975656553U;
+        static const AkUniqueID TRAP_SAW_ATTACK = 2188379821U;
+        static const AkUniqueID TRAP_SET_SUCCESS = 2011209205U;
+        static const AkUniqueID TRAP_SPIN_ATTACK = 970676282U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
+        static const AkUniqueID UI_MOUSEOVER = 3809715957U;
+        static const AkUniqueID USE_COIN = 2375532986U;
+    } // namespace EVENTS
+
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
